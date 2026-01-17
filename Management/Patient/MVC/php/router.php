2b401327ef8462ace0a7c66f436f201f3ac5729f
foejjj <22-49440-3@student.aiub.edu>
2026-01-17 22:55:31 +0600

    router

--- Management/Patient/MVC/php/router.php
+++ Management/Patient/MVC/php/router.php
@@ -1,7 +1,55 @@
 <?php
-function require_login_patient(): void {
-    if (!isset($_SESSION['user']) || ($_SESSION['user']['role'] ?? '') !== 'patient') {
-        header('Location: index.php?r=login');
-        exit;
-    }
+$r = $_GET['r'] ?? 'dashboard';
+$method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+
+switch ($r) {
+    case 'login':
+        if ($method === 'POST') AuthController::login();
+        else AuthController::showLogin();
+        break;
+
+    case 'register':
+        if ($method === 'POST') AuthController::register();
+        else AuthController::showRegister();
+        break;
+
+    case 'logout':
+        AuthController::logout();
+        break;
+
+    case 'dashboard':
+        require_login_patient();
+        PatientController::dashboard();
+        break;
+
+    case 'book_appointment':
+        require_login_patient();
+        if ($method === 'POST') PatientController::bookAppointmentSubmit();
+        else PatientController::bookAppointmentForm();
+        break;
+
+    case 'appointments':
+        require_login_patient();
+        PatientController::appointments();
+        break;
+
+    case 'cancel_appointment':
+        require_login_patient();
+        PatientController::cancelAppointment();
+        break;
+
+    case 'prescriptions':
+        require_login_patient();
+        PatientController::prescriptions();
+        break;
+
+    case 'download_latest_prescription':
+        require_login_patient();
+        PatientController::downloadLatestPrescription();
+        break;
+
+    default:
+        require_login_patient();
+        PatientController::dashboard();
+        break;
 }
