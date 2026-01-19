71658d774e79007fb89d9ce9dbe9f6142df4ac78
foejjj <22-49440-3@student.aiub.edu>
2026-01-19 18:25:12 +0600

    displays error or success messages

--- Management/Patient/MVC/html/book_appointment.php
+++ Management/Patient/MVC/html/book_appointment.php
@@ -1,2 +1,10 @@
 <?php include "_layout_top.php"; ?>
 <h2 class="page-title">Book Appointment</h2>
+
+<?php if(isset($_GET["err"])): ?>
+  <p style="color:red"><?= htmlspecialchars($_GET["err"]) ?></p>
+<?php endif; ?>
+
+<?php if(isset($_GET["msg"])): ?>
+  <p style="color:green"><?= htmlspecialchars($_GET["msg"]) ?></p>
+<?php endif; ?>
