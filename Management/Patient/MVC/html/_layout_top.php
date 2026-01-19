3f33b58b09741c6a3dff529cd98e6dcaebe1ed38
foejjj <22-49440-3@student.aiub.edu>
2026-01-19 14:25:23 +0600

    layout for patient

---
+++ Management/Patient/MVC/html/_layout_top.php
@@ -0,0 +1,6 @@
+<?php
+require_once("../../../Auth/MVC/db/db.php");
+if (!isset($_SESSION["role"]) || $_SESSION["role"] !== "patient") {
+  header("Location: /web-tech-project/Management/Auth/MVC/html/login.php");
+  exit;
+}
