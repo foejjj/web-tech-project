e15627a920fad52491dc40646d1725faaa1d16a6
foejjj <22-49440-3@student.aiub.edu>
2026-01-19 19:47:14 +0600

    select from database

--- Management/Doctor/MVC/html/appointments.php
+++ Management/Doctor/MVC/html/appointments.php
@@ -9,3 +9,14 @@
   <th>Patient</th>
   <th>Status</th>
 </tr>
+
+<?php
+$did = (int)$_SESSION["doctor_id"];
+$q = "
+  SELECT a.date,a.time,a.status,p.name
+  FROM appointments a
+  JOIN patients p ON p.id=a.patient_id
+  WHERE a.doctor_id = $did AND a.approved = 1
+  ORDER BY a.date DESC, a.time DESC
+";
+$res = $conn->query($q);
