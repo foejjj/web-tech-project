79b2a150da3e3ef02c5d732e09c18dc8e1a387f9
foejjj <22-49440-3@student.aiub.edu>
2026-01-19 20:55:55 +0600

    from db

--- Management/Doctor/MVC/php/profile_process.php
+++ Management/Doctor/MVC/php/profile_process.php
@@ -5,3 +5,7 @@ if (!isset($_SESSION["role"]) || $_SESSION["role"] !== "doctor") {
   header("Location: /web-tech-project/Management/Auth/MVC/html/login.php");
   exit;
 }
+
+$user_id = (int)($_SESSION["user_id"] ?? 0);
+$doctor_id = (int)($_SESSION["doctor_id"] ?? 0);
+$action = $_POST["action"] ?? "";
