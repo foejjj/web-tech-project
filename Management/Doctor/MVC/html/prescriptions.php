86e75db69d124508d05998347b5d46be52a40012
foejjj <22-49440-3@student.aiub.edu>
2026-01-19 19:52:03 +0600

    show message

--- Management/Doctor/MVC/html/prescriptions.php
+++ Management/Doctor/MVC/html/prescriptions.php
@@ -1,3 +1,11 @@
 <?php include "_layout_top.php"; ?>
 
 <h2 class="page-title">Prescriptions</h2>
+
+<?php if (isset($_GET["msg"])): ?>
+  <div class="alert success"><?= htmlspecialchars($_GET["msg"]) ?></div>
+<?php endif; ?>
+
+<?php if (isset($_GET["err"])): ?>
+  <div class="alert error"><?= htmlspecialchars($_GET["err"]) ?></div>
+<?php endif; ?>
