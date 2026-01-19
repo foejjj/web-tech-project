ec5880cf2ef0f9dcd171087bf21eb133579016b1
foejjj <22-49440-3@student.aiub.edu>
2026-01-19 18:49:01 +0600

    prescription show

---
+++ Management/Patient/MVC/html/prescriptions.php
@@ -0,0 +1,9 @@
+<?php
+require_once("../../../Auth/MVC/db/db.php");
+
+if (!isset($_SESSION["role"]) || $_SESSION["role"] !== "patient") {
+  header("Location: /web-tech-project/Management/Auth/MVC/html/login.php");
+  exit;
+}
+
+include "_layout_top.php";
