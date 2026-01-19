5a8c2c89d7f96953d504d511df2a1ef8b07579c9
foejjj <22-49440-3@student.aiub.edu>
2026-01-19 18:52:55 +0600

    displays prescription

--- Management/Patient/MVC/html/prescriptions.php
+++ Management/Patient/MVC/html/prescriptions.php
@@ -19,3 +19,34 @@ ORDER BY pr.created_at DESC
 ";
 $res = $conn->query($sql);
 ?>
+
+<h2 class="page-title">My Prescriptions</h2>
+
+<div class="table-card">
+  <table class="table">
+    <thead>
+      <tr>
+        <th style="width:180px;">Date</th>
+        <th style="width:150px;">Doctor</th>
+        <th>Prescription</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php if($res && $res->num_rows > 0): ?>
+        <?php while($row = $res->fetch_assoc()): ?>
+          <tr>
+            <td><?= htmlspecialchars($row["created_at"]) ?></td>
+            <td><?= htmlspecialchars($row["doctor_name"]) ?></td>
+            <td><?= nl2br(htmlspecialchars($row["medicines"])) ?></td>
+          </tr>
+        <?php endwhile; ?>
+      <?php else: ?>
+        <tr>
+          <td colspan="3">No prescriptions found.</td>
+        </tr>
+      <?php endif; ?>
+    </tbody>
+  </table>
+</div>
+
+<?php include "_layout_bottom.php"; ?>
