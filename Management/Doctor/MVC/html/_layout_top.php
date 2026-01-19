5bcda2c3088a9b5c3d9f10c9007d67fbe67c92c0
foejjj <22-49440-3@student.aiub.edu>
2026-01-19 19:31:42 +0600

    db file path

---
+++ Management/Doctor/MVC/html/_layout_top.php
@@ -0,0 +1,8 @@
+<?php
+require_once("../../../Auth/MVC/db/db.php");
+if (!isset($_SESSION["role"]) || $_SESSION["role"] !== "doctor") {
+  header("Location: /web-tech-project/Management/Auth/MVC/html/login.php");
+  exit;
+}
+$displayName = $_SESSION["name"] ?? "Doctor";
+?>
