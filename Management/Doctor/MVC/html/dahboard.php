43dd4fdcceca1296773613ce78ad4d3ee061f793
foejjj <22-49440-3@student.aiub.edu>
2026-01-19 19:40:55 +0600

    appointment button add

--- Management/Doctor/MVC/html/dahboard.php
+++ Management/Doctor/MVC/html/dahboard.php
@@ -1,3 +1,10 @@
 <?php include "_layout_top.php"; ?>
 
 <h2 class="page-title">Dashboard</h2>
+
+<div class="card-grid">
+  <div class="card">
+    <h3>Appointments</h3>
+    <p>View assigned patient appointments.</p>
+    <a class="btn" href="appointments.php">Open</a>
+  </div>
