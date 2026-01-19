0e539dd1d7c0b089d29e92a234d226670d408766
foejjj <22-49440-3@student.aiub.edu>
2026-01-19 20:55:34 +0600

    check role

--- Management/Doctor/MVC/php/profile_process.php
+++ Management/Doctor/MVC/php/profile_process.php
@@ -1,2 +1,7 @@
 <?php
 require_once("../../../Auth/MVC/db/db.php");
+
+if (!isset($_SESSION["role"]) || $_SESSION["role"] !== "doctor") {
+  header("Location: /web-tech-project/Management/Auth/MVC/html/login.php");
+  exit;
+}
